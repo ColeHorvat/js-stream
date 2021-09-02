3cb56c778cae5bc0fd2d94550b4bb63cded53646
Cole Horvat <ColeHorvat@gmail.com>
2021-09-01 20:11:32 -0700

    Finished running_late function

--- late.js
+++ late.js
@@ -1,1 +1,11 @@
-// start coding your function here!
+function running_late() {
+    let date = new Date();
+    let time = date.getHours();
+
+    if(time < 18)
+        return "It is still early!";
+    else
+        return "It is late!";
+}
+
+exports.running_late = running_late;
